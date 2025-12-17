67a797b3c5f938f799dfa65f5a17b29afbd2021d
Limkon <limkon@skvip.eu.org>
2025-12-17 09:32:06 +0800

    Create build.gradle.kts

---
+++ android/build.gradle.kts
@@ -0,0 +1,6 @@
+// 路径: android/build.gradle.kts
+// 请注意版本号，这里使用的是较新的稳定版本
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+}
