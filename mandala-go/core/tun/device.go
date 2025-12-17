78b6a0011197634fe461987e91c25f7e6822c967
Limkon <limkon@skvip.eu.org>
2025-12-17 20:26:01 +0800

    Update device.go

--- mandala-go/core/tun/device.go
+++ mandala-go/core/tun/device.go
@@ -5,13 +5,11 @@ import (
 	"os"
 	"sync/atomic"
 
-	// [修复] 引入 tcpip 包
-	"gvisor.dev/gvisor/pkg/tcpip"
+	"gvisor.dev/gvisor/pkg/tcpip" // 必須引入
 	"gvisor.dev/gvisor/pkg/tcpip/link/fdbased"
 	"gvisor.dev/gvisor/pkg/tcpip/stack"
 )
 
-// Device 包装了 Android 的 VPN 文件描述符
 type Device struct {
 	fd      int
 	mtu     uint32
@@ -20,19 +18,15 @@ type Device struct {
 	closed  uint32
 }
 
-// NewDevice 创建一个新的 TUN 设备包装器
 func NewDevice(fd int, mtu uint32) (*Device, error) {
 	if fd < 0 {
 		return nil, fmt.Errorf("invalid fd: %d", fd)
 	}
 
-	// 创建 gVisor 的基于 FD 的链路端点
 	ep, err := fdbased.New(&fdbased.Options{
 		FDs: []int{fd},
 		MTU: mtu,
-		// Android VPN 通常没有以太网头，是纯 IP 包
 		EthernetHeader: false, 
-		// 自定义关闭回调
 		ClosedFunc: func(e tcpip.Error) {},
 	})
 	if err != nil {
