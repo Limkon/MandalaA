d18990039da0853057d573dbc50efe1217780bad
Limkon <limkon@skvip.eu.org>
2025-12-17 09:20:38 +0800

    Create Color.kt

---
+++ android/app/src/main/java/com/example/mandala/ui/theme/Color.kt
@@ -0,0 +1,28 @@
+// 文件路徑: android/app/src/main/java/com/example/mandala/ui/theme/Color.kt
+
+package com.example.mandala.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// 淺色模式顏色
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Mandala 專屬配色 (深紫色/靛青色)
+val MandalaPrimary = Color(0xFF673AB7) // 深紫色
+val MandalaOnPrimary = Color(0xFFFFFFFF)
+val MandalaContainer = Color(0xFFEADDFF)
+
+val MandalaSecondary = Color(0xFF00BCD4) // 靛青色 (強調)
+val MandalaBackground = Color(0xFFF5F5F5)
+val MandalaSurface = Color(0xFFFFFFFF)
+
+// 深色模式適配
+val MandalaDarkPrimary = Color(0xFFD0BCFF)
+val MandalaDarkBackground = Color(0xFF121212)
+val MandalaDarkSurface = Color(0xFF1E1E1E)
