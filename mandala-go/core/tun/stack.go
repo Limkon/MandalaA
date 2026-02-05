fea0fd18a2b970e3d17957a963d3123ee1a3a421
Limkon <130622161+Limkon@users.noreply.github.com>
2026-02-05 12:25:28 +0800

    Update stack.go

--- mandala-go/core/tun/stack.go
+++ mandala-go/core/tun/stack.go
@@ -140,7 +140,7 @@ func (s *Stack) handleTCP(r *tcp.ForwarderRequest) {
 
 		client := protocol.NewMandalaClient(s.config.Username, password)
 		payload, hErr = client.BuildHandshakePayload(targetHost, targetPort, s.config.Settings.Noise)
-
+	
 	case "trojan":
 		payload, hErr = protocol.BuildTrojanPayload(s.config.Password, targetHost, targetPort)
 	case "vless":
@@ -296,9 +296,9 @@ func (s *Stack) handleRemoteDNS(localConn *gonet.UDPConn) {
 		if password == "" {
 			password = s.config.UUID
 		}
-
 		client := protocol.NewMandalaClient(s.config.Username, password)
 		payload, _ = client.BuildHandshakePayload("8.8.8.8", 53, s.config.Settings.Noise)
+
 	case "trojan":
 		payload, _ = protocol.BuildTrojanPayload(s.config.Password, "8.8.8.8", 53)
 	case "vless":
